0711386e41770f3a922566b4f4e3e8d43880c38b
system <system@ume.academy>
2019-07-25 14:37:06 -0700

    fixed install script

--- install.sh
+++ install.sh
@@ -9,17 +9,21 @@ if [ -d "$dir" ]
     else
         printf "Arduino device driver directory does not exist\n"
         printf "Creating at /home/ume/.arduino15\n\n"
-        mkdir /home/ume/.arduino15/ > /dev/null 2>&1
+        mkdir /home/ume/.arduino15/  > /dev/null 2>&1
         ## creates driver directory
 fi 
 
-mkdir /home/ume/tempclone /home/ume/tempclone/files > /dev/null 2>&1 ## create temporary directories for clone
+mkdir -p /home/ume/tempclone/files > /dev/null 2>&1 ## create temporary directories for clone
 printf "Downloading trinket drivers from GitHub\n\n"
-git clone https://github.com/kam-eeel/ume.git /home/ume/tempclone > /dev/null 2>&1 ## clone zip of files from github
+sleep 3
+git clone https://github.com/kam-eeel/ume  > /dev/null 2>&1 ## clone zip of files from github
 printf "Installing files to /home/ume/.arduino15\n\n"
+mv ./ume /home/ume/tempclone/
 unzip /home/ume/tempclone/ume/trinket-drivers.zip -d /home/ume/tempclone/files > /dev/null 2>&1 ## unzip to temp dir
 cp -n -r /home/ume/tempclone/files/* /home/ume/.arduino15 > /dev/null 2>&1 ## copy files to driver direcctory
 chown -R ume /home/ume/.arduino15 > /dev/null 2>&1 ## change ownership to user ume
+sleep 2
 printf "Cleaning up...\n\n"
 rm -rf /home/ume/tempclone > /dev/null 2>&1 ## remove clone files and temp directories
+sleep 2
 exit 0
